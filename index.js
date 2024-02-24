8a24edc73860de019e80715620787978e6125e82
水靈 <z23320785@gmail.com>
2024-02-25 04:57:45 +0800

    no message

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ import express from "express";
 //const express=require('express');
 
 import bodyParser from "body-parser";
-const app = express();
-const PORT = 9421;
 import GetStocksService from "./Services/GetStocksService.js";
 import HttpClientService from "./Services/HttpClientService.js";
 import StockController from "./Controllers/StockController.js"
+
+import swaggerUi from 'swagger-ui-express';
+// import swaggerDocument from './swagger.json';
+
+import swaggerDocument from './swagger.json' assert { type: 'json' };
+
+const app = express();
+const PORT = 9421;
 const HOST = "0.0.0.0"; // This will make the server accessible externally
 
+
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', StockController); // 使用新的路由
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());//Middlewares 解析傳入請求的 JSON 格式數據
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
