57dd8cae5d1e7897ac716248136dd09da81d22c2
水靈 <z23320785@gmail.com>
2024-02-27 00:58:13 +0800

    add  卷商分點進出

--- Controllers/StockController.js
+++ Controllers/StockController.js
@@ -3,6 +3,9 @@
 import express from 'express';
 import GetStocksService from '../Services/GetStocksService.js';
 const appRouter  = express.Router();
+import axios from "axios";
+import cheerio from "cheerio";
+import iconv from 'iconv-lite';
 
 
 /**
@@ -99,7 +102,107 @@ appRouter.get("/getFiveLevelsOfStockInformation/:stockNo", async (req, res) => {
   });
 
 
+/**
+ * @swagger
+ * /getSecuritiesCompanyTransactionRecords/{stockNo}:
+ *   get:
+ *     tags:
+ *         - Stock
+ *     summary: 卷商分點進出
+ *     parameters:
+ *       - in: path
+ *         name: stockNo
+ *         required: true
+ *         description: Stock No
+ *         schema:
+ *           type: string
+ *     description: Returns 卷商分點進出 data.
+ *     responses:
+ *       200:
+ *         description: Successful response with getSecuritiesCompanyTransactionRecords data.
+ */
+appRouter.get("/getSecuritiesCompanyTransactionRecords/:stockNo", async (req, res) => {
+  const stockNo = req.params.stockNo;
+  try {
+    const url = `https://fubon-ebrokerdj.fbs.com.tw/z/zc/zco/zco.djhtm?a=${stockNo}&e=2024-2-19&f=2024-2-19`;
 
+    const response = await axios.get(url, {
+      responseType: 'arraybuffer'  // 将响应类型设置为 arraybuffer
+  });
+  const htmlBuffer = response.data;
+  const html = iconv.decode(htmlBuffer, 'big5'); // 使用 iconv-lite 解码 Big5 编码
+
+  const $ = cheerio.load(html);
+    
+    // 选择所有符合条件的 <tr> 元素
+    const trElements = $('tr');
+    const dataArray = [];
+
+    // 定义用于存储结果的数组
+    const result = [];
+    console.log(
+      "%c getSecuritiesCompanyTransactionRecords",
+      "color:#BB3D00;font-family:system-ui;font-size:2rem;font-weight:bold",
+      // "tdContentArray",
+      // tdContentArray,
+      "trElements",
+      trElements,
+    );
+    function getTextFromTd(tdElement) {
+      const anchorElement = $(tdElement).find('a');
+      if (anchorElement.length > 0) {
+        return anchorElement.text().trim();
+      } else {
+        return $(tdElement).text().trim();
+      }
+    }
+    trElements.each((index, element) => {
+      // 获取当前 <tr> 元素下的所有 <td> 元素
+      const tdElements = $(element).find('td');
+      
+      // 如果 <td> 元素数量为 10，说明这是您想要的格式
+      if (tdElements.length === 10) {
+        // 创建一个对象来存储 <td> 元素的文本内容
+        const dataObject = {
+          securitiesDealer: $(tdElements[0]).text().trim(),
+          buyingIn: $(tdElements[1]).text().trim(),
+          sellingOut: $(tdElements[2]).text().trim(),
+          totalDifference: $(tdElements[6]).text().trim()-$(tdElements[7]).text().trim(), //$(tdElements[3]).text().trim(),
+          percentage: $(tdElements[4]).text().trim(),
+        };
+        const dataObject2 = {
+          securitiesDealer: $(tdElements[5]).text().trim(),
+          buyingIn: $(tdElements[6]).text().trim(),
+          sellingOut: $(tdElements[7]).text().trim(),
+          totalDifference: $(tdElements[6]).text().trim()-$(tdElements[7]).text().trim(), //$(tdElements[8]).text().trim(),
+          percentage: $(tdElements[9]).text().trim(),
+        };
+        if (dataObject.percentage !== "佔成交比重") {
+          dataArray.push(dataObject);
+        }
+
+        if (dataObject2.percentage !== "佔成交比重") {
+          dataArray.push(dataObject2);
+        }
+
+
+      }
+    });
+
+    dataArray.sort((a, b) => {
+      // 将百分比字符串转换为数字，去除百分号并转换为浮点数进行比较
+      const percentageA = parseFloat(a.percentage.replace('%', ''));
+      const percentageB = parseFloat(b.percentage.replace('%', ''));
+      // 比较百分比值并返回排序结果
+      return percentageB - percentageA;
+    });
+    res.json(dataArray);
+    //return dataArray;
+  } catch (error) {
+    // 如果请求发生错误，返回错误状态码和错误信息
+    res.status(500).json({ error: error.message });
+  }
+});
 
 
 
---
+++ Note/Reference Website.md
@@ -0,0 +1,3 @@
+
+# 券商分點
+- 參考網站 https://fubon-ebrokerdj.fbs.com.tw/z/zc/zco/zco.djhtm?a=5443&e=2024-2-20&f=2024-2-20
