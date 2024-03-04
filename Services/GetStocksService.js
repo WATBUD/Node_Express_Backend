de0900b6a162050740470cb879c601058bf154be
水靈 <z23320785@gmail.com>
2024-03-05 00:12:08 +0800

    move getTheLatestOpeningDate fn to services

--- Services/GetStocksService.js
+++ Services/GetStocksService.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import _ from "lodash";
+import cheerio from "cheerio";
+import iconv from 'iconv-lite';
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 class GetStocksService {
   //static httpClient = axios.create();
@@ -106,10 +108,6 @@ class GetStocksService {
     }
   }
 
-
-
-
-
   static async getTop20_SecuritiesByTradingVolume() {
     try {
       const latestOpeningDate = await this.getTheLatestOpeningDate();
@@ -166,6 +164,70 @@ class GetStocksService {
     }
   }
 
+  static async getSecuritiesCompanyTransactionRecords(stockNo) {
+    try {
+      const url = `https://fubon-ebrokerdj.fbs.com.tw/z/zc/zco/zco.djhtm?a=${stockNo}&e=2024-2-19&f=2024-2-19`;
+  
+      const response = await axios.get(url, {
+        responseType: "arraybuffer", // 将响应类型设置为 arraybuffer
+      });
+      const htmlBuffer = response.data;
+      const html = iconv.decode(htmlBuffer, "big5"); // 使用 iconv-lite 解码 Big5 编码
+      const $ = cheerio.load(html);
+      const trElements = $("tr");
+      const dataArray = [];
+      const result = [];
+      console.log(
+        "%c getSecuritiesCompanyTransactionRecords",
+        "color:#BB3D00;font-family:system-ui;font-size:2rem;font-weight:bold",
+        // "tdContentArray",
+        // tdContentArray,
+        "trElements",
+        trElements
+      );
+      trElements.each((index, element) => {
+        // 获取当前 <tr> 元素下的所有 <td> 元素
+        const tdElements = $(element).find("td");
+        // 如果 <td> 元素数量为 10，说明这是您想要的格式
+        if (tdElements.length === 10) {
+          // 创建一个对象来存储 <td> 元素的文本内容
+          const dataObject = {
+            securitiesDealer: $(tdElements[0]).text().trim(),
+            buyingIn: $(tdElements[1]).text().trim(),
+            sellingOut: $(tdElements[2]).text().trim(),
+            totalDifference:
+              $(tdElements[6]).text().trim() - $(tdElements[7]).text().trim(), //$(tdElements[3]).text().trim(),
+            percentage: $(tdElements[4]).text().trim(),
+          };
+          const dataObject2 = {
+            securitiesDealer: $(tdElements[5]).text().trim(),
+            buyingIn: $(tdElements[6]).text().trim(),
+            sellingOut: $(tdElements[7]).text().trim(),
+            totalDifference:
+              $(tdElements[6]).text().trim() - $(tdElements[7]).text().trim(), //$(tdElements[8]).text().trim(),
+            percentage: $(tdElements[9]).text().trim(),
+          };
+  
+          if (dataObject.percentage !== "佔成交比重") {
+            dataArray.push(dataObject);
+          }
+          if (dataObject2.percentage !== "佔成交比重") {
+            dataArray.push(dataObject2);
+          }
+        }
+      });
+      dataArray.sort((a, b) => {
+        const percentageA = parseFloat(a.percentage.replace("%", ""));
+        const percentageB = parseFloat(b.percentage.replace("%", ""));
+        return percentageB - percentageA;
+      });
+      //res.json(dataArray);
+      return dataArray;
+    } catch (error) {
+      //res.status(500).json({ error: error.message });
+      return error.message;
+    }
+  }
 
   static async getTheLatestOpeningDate() {
     try {
@@ -202,6 +264,9 @@ class GetStocksService {
       return "发生异常：" + error.message;
     }
   }
+
+
+
 
   static async getStockMarketOpeningAndClosingDates(requestAllData = false) {
     try {
